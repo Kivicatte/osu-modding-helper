9ed25eb0869c97e3f61d4beda2b2af1c5469ac30
Kivicatte <vl777nd@list.ru>
2026-03-18 08:55:57 +0300

    Turned lines on graph into interactive UI elements

--- src/playtest/graph.py
+++ src/playtest/graph.py
@@ -2,23 +2,125 @@ from PySide6.QtCore import Qt
 
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backend_bases import MouseEvent, PickEvent
 import numpy as np
-from enum import StrEnum
 
-from ..beatmap import StrainsData
+from enum import Enum
+from dataclasses import dataclass, field
+from typing import ClassVar
 
-
-class GraphColors(StrEnum):
-    PLOT = '#ccaa33'
-    CURSOR = '#68c627'
-    BOOKMARK = '#4a90e2'
-    MISS = '#cc2222'
+from ..beatmap.data import StrainsData
+from .base import CommentUIElement, CommentUIContainer, CommentType, timed_comment_types
 
 
-class StrainGraph(FigureCanvas):
+@dataclass
+class LineStyle:
+    default: ClassVar[dict] = {
+        'linewidth': 1,
+        'linestyle': '-',
+        'alpha': 0.9
+    }
+
+    base: dict = field(default_factory=dict)
+    active: dict = field(default_factory=dict)
+
+    def __post_init__(self):
+        self.base = self.default | self.base
+        self.active = self.base | self.active
+
+
+class LineStyles(Enum):
+    PLOT = LineStyle(
+        base={'color': '#ccaa33'}
+    )
+    CURSOR = LineStyle(
+        base={'color': '#68c627', 'linewidth': 2}
+    )
+    BOOKMARK = LineStyle(
+        base={'color': '#4a90e2'},
+        active={'linewidth': 2}
+    )
+    MISS = LineStyle(
+        base={'color': '#cc2222'},
+        active={'linewidth': 2}
+    )
+
+
+comment_styles = {
+    CommentType.TIMELINE: LineStyles.BOOKMARK.value,
+    CommentType.MISS: LineStyles.MISS.value
+}
+
+
+class VLine(CommentUIElement):
+    def __init__(self, time_ms: int, type_: CommentType, canvas: FigureCanvas):
+        self._time_ms = time_ms
+        self._canvas = canvas
+
+        style = comment_styles[type_]
+        self._style = style
+        self._focused = False
+
+        self._line = canvas.figure.axes[0].axvline(
+            x=time_ms,
+            zorder=9,
+            picker=3,
+            **self._style.base
+        )
+
+        self._cids = [
+            canvas.mpl_connect('motion_notify_event', self._on_motion),
+            canvas.mpl_connect('pick_event', self._on_pick)
+        ]
+
+        super().__init__(type_, parent=None)
+
+    def set_type(self, type_: CommentType):
+        style = comment_styles[type_]
+        self._style = style
+
+        super().set_type(type_)
+
+    def _set_focus(self, focused: bool = True):
+        if self._focused == focused:
+            return
+        self._focused = focused
+        self._redraw()
+
+    def _on_motion(self, event: MouseEvent):
+        if event.inaxes != self._line.axes:
+            self._set_focus(False)
+            return
+
+        contains, _ = self._line.contains(event)
+        self._set_focus(contains)
+
+    def _on_pick(self, event: PickEvent):
+        if event.artist is self._line:
+            self.on_toggle()
+
+    def _redraw(self):
+        style = self._style.active if self.property('active') or self._focused else self._style.base
+        self._line.set(**style)
+        self._canvas.draw_idle()
+
+    def deleteLater(self):
+        for cid in self._cids:
+            self._canvas.mpl_disconnect(cid)
+        self._line.remove()
+        super().deleteLater()
+
+
+class StrainGraph(CommentUIContainer, FigureCanvas):
     def __init__(self):
         self.figure = Figure(facecolor='none')
-        super().__init__(self.figure)
+
+        FigureCanvas.__init__(self, self.figure)
+        CommentUIContainer.__init__(
+            self,
+            factory=lambda *args: VLine(*args, canvas=self),
+            filter_=lambda t, type_: type_ in timed_comment_types
+        )
 
         self.axes = self.figure.add_subplot()
         self.axes.set_facecolor('none')
@@ -36,6 +138,8 @@ class StrainGraph(FigureCanvas):
         self.setAttribute(Qt.WA_TranslucentBackground)
         self.setStyleSheet('background-color: transparent;')
 
+        self._plot_style = LineStyles.PLOT.value
+        self._cursor_style = LineStyles.CURSOR.value
         self._pointer = None
         self._bookmarks = {}
 
@@ -49,11 +153,8 @@ class StrainGraph(FigureCanvas):
         if self._pointer is None:
             self._pointer = self.axes.axvline(
                 x=time,
-                color=GraphColors.CURSOR,
-                linewidth=2,
-                linestyle='-',
-                alpha=0.9,
-                zorder=10
+                zorder=10,
+                **self._cursor_style.base
             )
         else:
             self._pointer.set_xdata([time, time])
@@ -67,44 +168,6 @@ class StrainGraph(FigureCanvas):
 
         self.draw()
 
-    def add_bookmark(self, time: int, /, color=GraphColors.BOOKMARK):
-        bookmark = self.axes.axvline(
-            x=time,
-            color=color,
-            linewidth=1.2,
-            linestyle='-',
-            alpha=0.9,
-            zorder=9
-        )
-
-        self._bookmarks[time] = bookmark
-        self.draw()
-
-    def add_miss_mark(self, time: int):
-        self.add_bookmark(time, color=GraphColors.MISS)
-
-    def remove_bookmark(self, time: int = None):
-        if time is None:
-            time = self.pointer_position
-        if time in self._bookmarks:
-            self._bookmarks[time].remove()
-            del self._bookmarks[time]
-            self.draw()
-
-    def set_bookmark_visibility(self, time: int = None, visible: bool = True):
-        if time is None:
-            time = self.pointer_position
-        if time in self._bookmarks:
-            self._bookmarks[time].set_visible(visible)
-            self.draw()
-
-    def clear_bookmarks(self):
-        for bookmark in self._bookmarks.values():
-            bookmark.remove()
-        self._bookmarks.clear()
-
-        self.draw()
-
     def plot(self, strains: StrainsData):
         if len(strains.xaxis) < 2:
             return
@@ -112,7 +175,7 @@ class StrainGraph(FigureCanvas):
         strains = strains.strains
 
         self.axes.cla()
-        self.axes.plot(time_points, strains, color=GraphColors.PLOT)
+        self.axes.plot(time_points, strains, **self._plot_style.base)
 
         # TODO: handle loooooooooooooooooooong maps
         self.axes.set_xlim(-10, time_points[-1] + 10)
